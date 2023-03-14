fabbde268002df5250fb9ea577a65abab5303aaf
aditisharma6202 <111963350+aditisharma6202@users.noreply.github.com>
2023-03-14 21:11:38 +0530

    nested navigation update

--- src/HeaderFooter/header.js
+++ src/HeaderFooter/header.js
@@ -1,113 +1,207 @@
-import {React, useState} from 'react';
-import { Link } from 'react-router-dom';
-
-const Navbutton=({title,icon,data})=>{
-    const[dropdown , setDropdown]=useState(false)
+import { React, useState } from "react";
+import { Link } from "react-router-dom";
+import { data } from "./headerData";
+//nested nesteddropdown data
+const NestedDropdownData=({info})=>{
+    //console.log('nested info'+  JSON.stringify(info.data))
+    //console.log('nested info key'+Object.keys(info.data) )
+    const nestedArray = info.data[Object.keys(info.data)]
+    console.log('nestedArray'+ nestedArray)
    
     return(
-        <div >
-       <button onClick={()=>{setDropdown(!dropdown)}} className='btn p-3 px-4 d-flex justify-content-between align-item-center text-center nav-link' >
-            <div className='d-flex gap-2'>
-           {icon}
-            <div>{title}</div>
-            </div>
-            <div>
-                {dropdown?<i class="bi bi-chevron-down"></i>:<i class="bi bi-chevron-up"></i>}
-            </div>
-        </button>
-        {dropdown&&
-        data.map((info)=>{return( <Link to={`/${info.navlink}`} className='nav-link d-flex gap-2 p-3 px-4 bg-light-blue' >{info.icon}{info.title}</Link>);})}
-          
-        
-        </div>
-        );
-}
-
-const Navlink=( {isOpen})=>{
-    const salesData=[{icon:<i class="bi bi-people-fill"></i>,title:'Customer',navlink:'/'},
-    {icon:<i class="bi bi-file-earmark-break-fill"></i>, title:'Quotions',navlink:'/'},
-    {icon:<i class="bi bi-card-text"></i>, title:'Invoices',navlink:'/'},
-    {icon:<i class="bi bi-globe-central-south-asia"></i>, title:'E-Invoices',navlink:'/'},
-    {icon:<i class="bi bi-receipt-cutoff"></i>, title:'E-Way bills',navlink:'/'},
-    {icon:<i class="bi bi-stickies-fill"></i>, title:'Sales Orders',navlink:'/'},
-    {icon:<i class="bi bi-card-heading"></i>, title:'Delivery Challans',navlink:'/'},
-    {icon:<i class="bi bi-credit-card-fill"></i>, title:'Credit notes',navlink:'/'},
-    {icon:<i class="bi bi-currency-rupee"></i>, title:'Receivables',navlink:'/'},
-    {icon:<i class="bi bi-clock-fill"></i>, title:'Payment received',navlink:'/'}
-
-]
-const InventoryData=[{icon:<i class="bi bi-basket3-fill"></i>,title:'Product',navlink:'product'},
-{icon:<i class="bi bi-file-earmark-break-fill"></i>, title:'Item Groups',navlink:'product'},
-{icon:<i class="bi bi-grid-fill"></i>, title:'Stocks/Inventory',navlink:'product'},
-{icon:<i class="bi bi-calculator-fill"></i>, title:'Stocks Adjustments',navlink:'product'},
-]
-
-const PurchaseData=[{icon:<i class="bi bi-basket3-fill"></i>,title:'Item List',navlink:'/'},
-{icon:<i class="bi bi-file-earmark-break-fill"></i>, title:'Item Groups',navlink:'/'},
-{icon:<i class="bi bi-grid-fill"></i>, title:'Stocks/Inventory',navlink:'/'},
-{icon:<i class="bi bi-calculator-fill"></i>, title:'Stocks Adjustments',navlink:'/'},
-]
-    
-    
-    return(
-    <div className={`navlink ${isOpen?'':'inview '}`}>
-        <Navbutton title={'Sales'} icon={ <i class="bi bi-graph-up-arrow"></i>} data={salesData}/>
-        <Navbutton title={'Inventory'} icon={ <i class="bi bi-box-seam-fill"></i> }data={InventoryData}/>
-        <Navbutton title={'Purchases'} icon={ <i class="bi bi-cart-check-fill"></i>} data={PurchaseData}/>
-        <Navbutton title={'Master data'} icon={ <i class="bi bi-clipboard-data-fill"></i>}data={PurchaseData}/>
-        <Navbutton title={'Other'} icon={ <i class="bi bi-motherboard-fill"></i>}data={PurchaseData}/>
-        <Navbutton title={'Accounting'} icon={ <i class="bi bi-book-half"></i>}data={PurchaseData}/>
-       <Link to={'/'} className='nav-link d-flex gap-2 p-3' ><i class="bi bi-grid-3x2-gap-fill px-2"></i>Report</Link>
-       <Link to={'/'} className='nav-link d-flex gap-2 p-3' ><i class="bi bi-grid-1x2-fill px-2"></i>Dashboard</Link>
-    </div>);
-}
-const LogoutMenu=({setSidemenu, menu})=>{
-   
-    return(
-    <div className='logout-menu' onMouseLeave={()=>{setSidemenu(false)}}>
-        {menu.map( (info)=>{
-            return(<Link to={'/'} className=' d-flex gap-2 p-3 link'>{info.icon}{info.title}</Link>);
+    <div className="nested-dropdown-menu">
+       {
+        nestedArray.map((info)=>{
+            return(<>
+            <div className="nested-dropdown-menu-link p-2"> {info.title}</div>
+            </>);
         })
        }
-         
+
     </div>);
 }
-function Header() {
-const[isOpen,setisOpen]=useState(true)
-const[isSidemenu , setSidemenu]= useState(false);
-const menu=[{icon:<i class="bi bi-house-check-fill"></i>, title:'Comapny Name'},
-{icon:<i class="bi bi-people-fill"></i>, title:'Manage User'},
-{icon:<i class="bi bi-gear-fill"></i>, title:'App Settings'},
-{icon:<i class="bi bi-power text-danger"></i>, title:<span className='text-danger'>Logout</span>}]
-    return (
-     <div className='header'>
-       
-        <div className='display-flex justify-content-between text-center pt-1 px-2'>
-            <div classname='d-flex text-center text-light' style={{fontSize:'1.2rem', display:'flex' , gap:'1rem'}}>
-                
-                <button className='btn p-0 fw-bolder' onClick={()=>{setisOpen(!isOpen)}} > 
-                {isOpen?<i class="bi bi-x-lg text-light"></i>:<i class="bi bi-list text-light"></i>} </button>
-                <div className='logo text-light' >LOGO</div>
-                
-            </div>
-            <div className='d-flex justify-content-between align-item-center gap-4'>
-                <button className='btn p-0 text-light'><i class="bi bi-search"></i></button>
-                <button className='btn p-0 text-light'><i class="bi bi-tag-fill"></i></button>
-                <button className='btn p-0 text-light'><i class="bi bi-bell-fill"></i></button>
-                {menu.map((info)=>{return(<>
-                    <button className='btn p-0 text-light only-desktop'>{info.icon}</button>
-                </>)})}
-                
-               
-                <button className='btn p-0 text-light only-mobile' onMouseEnter={()=>{setSidemenu(true)}}
-                ><i class="bi bi-three-dots-vertical"></i></button>
-            </div>
+//dropdown data and nested dropdown
+const NestedDropdown=({info})=>{
+ const [opennesteddropdown , setOpennesteddropdown]=useState(false)
 
+ if(info.icon){
+    return(  <Link
+    to={`/${info.link}`}
+    className="nav-link d-flex gap-2  px-4 bg-light-blue justify-content-between"
+  >
+    <div>
+      <span className="p-2">{info.icon}</span>
+      {info.name}
+    </div>
+  </Link>)
+ }
+   else{
+    return(
+        <div className="nav-link d-flex gap-2  px-4 bg-light-blue justify-content-between nested-dropdown-btn">
+  <div>
+
+      {info.name}
+    </div>
+        {info.icon ? "" :
+        <div >
+        <button className="btn p-0" onClick={()=>{setOpennesteddropdown(!opennesteddropdown) }}>
+          <i class="bi bi-caret-right-fill"></i>
+        </button>
         </div>
-       <Navlink isOpen={isOpen}/>
-       {isSidemenu && <LogoutMenu setSidemenu={setSidemenu} menu={menu}/>}
-       
-    </div> );
+        }
+        {opennesteddropdown&&<NestedDropdownData info={info}/>}
+        </div>
+        
+    )
+   }
+}
+
+//dropdowns here
+const Navbutton = ({ data }) => {
+  const [dropdown, setDropdown] = useState(false);
+  const dropdowndata = data.data[Object.keys(data.data)];
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setDropdown(!dropdown);
+        }}
+        className="btn p-3 px-4 d-flex justify-content-between align-item-center text-center nav-link"
+      >
+        <div className="d-flex gap-2">
+          {data.icon}
+          <div>{data.title}</div>
+        </div>
+        <div>
+          {dropdown ? (
+            <i class="bi bi-chevron-up"></i>
+          ) : (
+            <i class="bi bi-chevron-down"></i>
+          )}
+        </div>
+      </button>
+
+      {dropdown &&
+        dropdowndata.map((info) => {
+            
+          return (
+            <>
+              <NestedDropdown info={info} />
+            </>
+          );
+        })}
+    </div>
+  );
+};
+
+const Navlink = ({ isOpen }) => {
+  return (
+    <div className={`navlink ${isOpen ? "" : "inview "}`}>
+      {data.map((info) => {
+        return (
+          <>
+            <Navbutton data={info} />
+          </>
+        );
+      })}
+
+      <Link to={"/"} className="nav-link d-flex gap-2 p-3">
+        <i class="bi bi-grid-3x2-gap-fill px-2"></i>Report
+      </Link>
+      <Link to={"/"} className="nav-link d-flex gap-2 p-3">
+        <i class="bi bi-grid-1x2-fill px-2"></i>Dashboard
+      </Link>
+    </div>
+  );
+};
+const LogoutMenu = ({ setSidemenu, menu }) => {
+  return (
+    <div
+      className="logout-menu"
+      onMouseLeave={() => {
+        setSidemenu(false);
+      }}
+    >
+      {menu.map((info) => {
+        return (
+          <Link to={"/"} className=" d-flex gap-2 p-3 link">
+            {info.icon}
+            {info.title}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+function Header() {
+  const [isOpen, setisOpen] = useState(true);
+  const [isSidemenu, setSidemenu] = useState(false);
+  const menu = [
+    { icon: <i class="bi bi-house-check-fill"></i>, title: "Comapny Name" },
+    { icon: <i class="bi bi-people-fill"></i>, title: "Manage User" },
+    { icon: <i class="bi bi-gear-fill"></i>, title: "App Settings" },
+    {
+      icon: <i class="bi bi-power text-danger"></i>,
+      title: <span className="text-danger">Logout</span>,
+    },
+  ];
+  return (
+    <div className="header">
+      <div className="display-flex justify-content-between text-center pt-1 px-2">
+        <div
+          classname="d-flex text-center text-light"
+          style={{ fontSize: "1.2rem", display: "flex", gap: "1rem" }}
+        >
+          <button
+            className="btn p-0 fw-bolder"
+            onClick={() => {
+              setisOpen(!isOpen);
+            }}
+          >
+            {isOpen ? (
+              <i class="bi bi-x-lg text-light"></i>
+            ) : (
+              <i class="bi bi-list text-light"></i>
+            )}{" "}
+          </button>
+          <div className="logo text-light">LOGO</div>
+        </div>
+        <div className="d-flex justify-content-between align-item-center gap-4">
+          <button className="btn p-0 text-light">
+            <i class="bi bi-search"></i>
+          </button>
+          <button className="btn p-0 text-light">
+            <i class="bi bi-tag-fill"></i>
+          </button>
+          <button className="btn p-0 text-light">
+            <i class="bi bi-bell-fill"></i>
+          </button>
+          {menu.map((info) => {
+            return (
+              <>
+                <button className="btn p-0 text-light only-desktop">
+                  {info.icon}
+                </button>
+              </>
+            );
+          })}
+
+          <button
+            className="btn p-0 text-light only-mobile"
+            onMouseEnter={() => {
+              setSidemenu(true);
+            }}
+          >
+            <i class="bi bi-three-dots-vertical"></i>
+          </button>
+        </div>
+      </div>
+      <Navlink isOpen={isOpen} />
+      {isSidemenu && <LogoutMenu setSidemenu={setSidemenu} menu={menu} />}
+    </div>
+  );
 }
 
 export default Header;
---
+++ src/HeaderFooter/headerData.js
@@ -0,0 +1,118 @@
+ //dropdown nested data 
+
+//dropdown nested under master
+//dropdown for account
+const nestedAccountData=[{title:'Group Type', link:'/'},{title:'Group', link:'/'},{title:'Tax Group', link:'/'},{title:'Leadger', link:'/'},{title:'HSN', link:'/'},
+{title:'Bank', link:'/'}
+]
+
+const nestedInventoryData=[
+    {title:'Product', link:'/'},
+    {title:'Category', link:'/'},{title:'Group Type', link:'/'},
+    {title:'Brand', link:'/'},
+    {title:'Group', link:'/'},{title:'Group Type', link:'/'},
+    {title:'Pack', link:'/'},
+    {title:'Unit', link:'/'},
+    {title:'Rack', link:'/'},
+]
+const nestedHRData=[
+    {title:'Department', link:'/'},
+    {title:'Designation', link:'/'},
+    {title:'Employee', link:'/'},
+]
+//nested dropdowns of master ends here
+
+
+//nested dropdowns of transaction starts here
+const nestedGRNData=[
+    {title:'New', link:'/'},
+    {title:'Edit', link:'/'},
+]
+const nestedPurchaseInvoiceData=[
+    {title:'New', link:'/'},
+    {title:'Edit', link:'/'},
+]
+const nestedSaleInvoiceData=[
+    {title:'New', link:'/'},
+    {title:'Edit', link:'/'},
+]
+const nestedSaleReturn=[
+    {title:'New', link:'/'},
+    {title:'Edit', link:'/'},
+]
+//nested dropdown of transaction ends here
+
+//nested dropdown of Accound starts here
+const nestedPayment=[
+    {title:'Payment', link:'/'},
+]
+
+//nested dropdown of Report starts here
+const nestedDesignData=[
+    {title:'New', link:'/'},
+    {title:'Edit', link:'/'},
+]
+const nestedSaleInvoicenData=[
+    {title:'New', link:'/'},
+    {title:'Edit', link:'/'},
+]
+ 
+//dropdown data
+const dashboardData=[{name:'Inventory',icon:<i class="bi bi-basket3-fill"></i>,link:'/'},
+{name:'Demo Power BI', icon:<i class="bi bi-file-earmark-break-fill"></i>,link:'/' }
+]
+
+const inventoryData=[
+    {name:'Account' ,data:{nestedAccountData}},
+    {name:'Inventory',data:{nestedInventoryData}},
+    {name:'H.R.', data:{nestedHRData}}
+]
+const transactionData=[
+    {name:'GRN',data:{nestedGRNData}},
+    {name:'Purchase Invoice',data:{nestedPurchaseInvoiceData}},
+    {name:'Sale Invoice',data:{nestedSaleInvoiceData}},
+    {name:'Sale Return',data:{nestedSaleReturn}},
+
+]
+const accountData=[
+    {name:'Payment',data:{nestedPayment}}
+]
+const counterData=[
+    {name:'Module 1' ,icon:<i class="bi bi-card-text"></i>, link:'/'},
+    {name:'Module 2' ,icon:<i class="bi bi-card-text"></i>, link:'/'},
+    {name:'Module 3' ,icon:<i class="bi bi-card-text"></i>,link:'/'}
+
+]
+const configData=[
+    {name:'Database' ,icon:<i class="bi bi-globe-central-south-asia"></i>, link:'/'},
+    {name:'Import' ,icon: <i class="bi bi-card-heading"></i>, link:'/'},
+    {name:'Error Log' ,icon:<i class="bi bi-globe-central-south-asia"></i>, link:'/'},
+]
+const webData=[
+    {name:'Web',icon:<i class="bi bi-globe-central-south-asia"></i>, link:'/'},
+    {name:'Import',icon: <i class="bi bi-card-heading"></i>, link:'/'},
+    {name:'Error Log',icon:<i class="bi bi-globe-central-south-asia"></i>, link:'/'},
+
+]
+const reportData=[
+    {name:'Design',data:{nestedDesignData}},
+    {name:'Sale Invoice',data:{nestedSaleInvoicenData}},
+]
+
+///navigation
+ 
+ export const data=[
+    {title:'Dashboard' ,icon: <i class="bi bi-graph-up-arrow"></i>, data:{dashboardData}},
+    { title:'Master', icon: <i class="bi bi-box-seam-fill"></i> ,data :{inventoryData}, type:'NestedDropdown'},
+    { title:'Transaction', icon: <i class="bi bi-cart-check-fill"></i> ,data:{transactionData}, type:'NestedDropdown' },
+   {title:'Account', icon: <i class="bi bi-clipboard-data-fill"></i>,data:{accountData} ,type:'NestedDropdown' }, 
+   {title:'Counter', icon: <i class="bi bi-motherboard-fill"></i>,data:{counterData} },
+   {title:'Report', icon: <i class="bi bi-book-half"></i>,data:{reportData},type:'NestedDropdown'} ,
+   {title:'Config', icon: <i class="bi bi-grid-3x2-gap-fill"></i>,data:{configData}} ,
+   {title:'WEB/APP', icon: <i class="bi bi-grid-3x2-gap-fill"></i>,data:{webData} } 
+   
+]
+
+
+//-----DATA ENDS HERE---
+
